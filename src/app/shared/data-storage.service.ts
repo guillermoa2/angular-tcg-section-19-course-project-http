dc0281c46c3fc9d4aed044588275ac9f987db84d
Guillermo Acosta <guillermo222@att.net>
2021-07-28 14:33:40 -0700

    Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,24 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 
+import { RecipeService } from "../recipes/recipe.service";
+
+
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        // subscribe here b/c the header component will NOT be interested in whether the request is done or not.
+        // The component has no interest in a response.
+        this.http
+            .put(
+                'https://angular-tcg-sec-19-crsprj-http-default-rtdb.firebaseio.com/recipes.json',
+                recipes
+            )
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
 }
